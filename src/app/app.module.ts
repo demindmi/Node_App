d05a7fc4f213b89ebbdc55094f07bec06ec03fc3
Dmitry Demin <dmitrydemin@Dmitrys-MacBook-Pro.local>
2018-11-27 00:36:21 -0500

    part 2 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -22,6 +23,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     ShoppingEditComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input ,Output,EventEmitter} from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe:Recipe; 
+  @Output() recipeSelectedEvent = new EventEmitter<void>();
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
+  }
+
+  recipeClicked(clickedRecipe){
+   this.recipeSelectedEvent.emit();
   }
 
 }
