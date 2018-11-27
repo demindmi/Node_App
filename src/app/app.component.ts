e6ad9926fbb52046cbd8874e61f75e47fc2770eb
Dmitry Demin <dmitrydemin@Dmitrys-MacBook-Pro.local>
2018-11-26 23:28:12 -0500

    step 1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pageToLoad:string = 'recipe'
+
+  render(pageName:string){
+    this.pageToLoad = pageName;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter,Output } from '@angular/core';
  
 @Component({
     selector   : "app-header",
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent{
-    
+    @Output() menuClicked = new EventEmitter <string>();
 
+
+    onSelect(page:string){
+        this.menuClicked.emit(page);
+    }
 }
